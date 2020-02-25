d58aa5fb07fcbb0e99d192458e55b7dcacfbe364
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-02-25 15:06:03 +0200

    rest django task 06

---
+++ task06_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dell\desktop\development\django_rest\rest_task_06\task06_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
